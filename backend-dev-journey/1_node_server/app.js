4256e579e1863d96db685dcc132371196b272803
vikashgitkit <vikashofficial9@gmail.com>
2026-02-13 18:04:09 +0530

    returning response

--- backend-dev-journey/1_node_server/app.js
+++ backend-dev-journey/1_node_server/app.js
@@ -1,8 +1,33 @@
 import { createServer } from 'http';
 
 const server = createServer((req, res)=> {
-    console.log("Req received:", req);
-    process.exit();//stops event loop
+    console.log("Req received:", req.method, req.url, req.headers);
+    // process.exit();//stops event loop
+
+    if(req.url === '/'){
+       res.setHeader('Content-Type', 'text/html');
+       res.write('<html>');
+       res.write('<head><title>Complete node backend</title></head>');
+       res.write('<body><h1>This is Home route</h1></body>')
+       res.write('</html>')
+       res.end();
+       return
+    } else if (req.url === '/products') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Complete node backend</title></head>');
+        res.write('<body><h1>This is product</h1></body>')
+        res.write('</html>')
+        res.end();
+        return
+    }
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Complete node backend</title></head>');
+        res.write('<body><h1>This is default</h1></body>')
+        res.write('</html>')
+        res.end();
+    
 });
 
 const PORT = 3003;
