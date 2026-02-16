ad4b2ee60ca43111b57cef52921a14b78f96849e
vikashgitkit <vikashofficial9@gmail.com>
2026-02-16 19:57:57 +0530

    express middleware

--- backend-dev-journey/2_express_v1/app.js
+++ backend-dev-journey/2_express_v1/app.js
@@ -3,9 +3,17 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 //Middleware
 app.use((req, res, next) => {
-    console.log("This is middleware....");
+    const name = req.body
+    console.log("Name body:", name)
+    console.log("This is middleware....", name.name);
+    return res.json({
+      msg: "This is middleware response",
+      name: name.name,
+    })
 })
 
 app.listen(PORT, ()=> {
