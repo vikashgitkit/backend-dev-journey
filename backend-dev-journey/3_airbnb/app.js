c7bb55008a3f99c786fe56cb726fcf0f5ed7a789
vikashgitkit <vikashofficial9@gmail.com>
2026-02-16 23:00:11 +0530

    express router

--- backend-dev-journey/3_airbnb/app.js
+++ backend-dev-journey/3_airbnb/app.js
@@ -15,6 +15,10 @@ app.use(express.urlencoded());
 app.use(userRouter);
 app.use(hostRouter);
 
+app.use((req,res,next)=> {
+    res.status(404).send("<h1>Your page is not found</h1>")
+})
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
