0312fa06a04f505f68f5c22d9d7a5eab63db2ef2
vikashgitkit <vikashofficial9@gmail.com>
2026-02-16 22:32:59 +0530

    express router

--- backend-dev-journey/3_airbnb/app.js
+++ backend-dev-journey/3_airbnb/app.js
@@ -2,13 +2,26 @@ const express = require('express');
 
 const app = express()
 
+app.use((req,res, next)=> {
+    console.log(req.url, req.method);
+    next();
+})
+
 app.get('/',(req, res, next) => {
-    console.log(req.url, res.method);
     res.send(`
         <h1>Welcome to airbnb</h1>
         <a href="/add-home">Add Home</a>
-        `);
-    
+        `); 
+});
+
+app.get('/add-home',(req, res, next) => {
+    res.send(`
+        <h1>Register your home here:</h1>
+        <form action="/add-home" method="POST">
+           <input type="text" name="houseName" placeholder="Enter the name of your house" />
+           <input type="submit"  />
+        </form>
+        `); 
 })
 
 const PORT = 3000;
