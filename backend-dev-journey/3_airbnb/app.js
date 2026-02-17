4d6e287735c1f1825197d33b18f40126e973480a
vikashgitkit <vikashofficial9@gmail.com>
2026-02-17 13:12:49 +0530

    added css in nodejs

--- backend-dev-journey/3_airbnb/app.js
+++ backend-dev-journey/3_airbnb/app.js
@@ -1,22 +1,27 @@
-const path = require('path')
+// Core Module
+const path = require('path');
 
+// External Module
 const express = require('express');
-const userRouter = require('./routes/userRouter')
-const hostRouter = require('./routes/hostRouter')
-const rootDir = require('./utils/pathUtil')
 
-const app = express()
+//Local Module
+const userRouter = require("./routes/userRouter")
+const hostRouter = require("./routes/hostRouter")
+const rootDir = require("./utils/pathUtil");
 
-app.use(express.urlencoded({ extended: true }));
+const app = express();
 
+app.use(express.urlencoded());
 app.use(userRouter);
-app.use('/host',hostRouter);
+app.use("/host", hostRouter);
 
-app.use((req,res,next)=> {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
+app.use(express.static(path.join(rootDir, 'public')))
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 })
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port: ${PORT}`);
-})
+  console.log(`Server running on address http://localhost:${PORT}`);
+});
--- backend-dev-journey/3_airbnb/routes/hostRouter.js
+++ backend-dev-journey/3_airbnb/routes/hostRouter.js
@@ -1,16 +1,17 @@
 const path = require('path')
+const rootDir = require("../utils/pathUtil")
 
 const express = require('express');
 const hostRouter = express.Router();
 
 
 hostRouter.get('/add-home',(req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'addHome.html'));
+    res.sendFile(path.join(rootDir, 'views', 'addHome.html'));
 })
 
 hostRouter.post('/add-home',(req, res, next) => {
     console.log(req.body);
-    res.sendFile(path.join(__dirname, '../', 'views', 'homeAdded.html'));
+    res.sendFile(path.join(rootDir, 'views', 'homeAdded.html'));
     
 })
 module.exports = hostRouter;
