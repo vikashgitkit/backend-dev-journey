2116c75dcd5b64b55935b1545e84ae722728c983
vikashgitkit <vikashofficial9@gmail.com>
2026-02-17 12:37:01 +0530

    Serinv hrml files in node js

--- backend-dev-journey/3_airbnb/app.js
+++ backend-dev-journey/3_airbnb/app.js
@@ -3,6 +3,7 @@ const path = require('path')
 const express = require('express');
 const userRouter = require('./routes/userRouter')
 const hostRouter = require('./routes/hostRouter')
+const rootDir = require('./utils/pathUtil')
 
 const app = express()
 
@@ -12,7 +13,7 @@ app.use(userRouter);
 app.use('/host',hostRouter);
 
 app.use((req,res,next)=> {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
 })
 
 const PORT = 3000;
--- backend-dev-journey/3_airbnb/routes/userRouter.js
+++ backend-dev-journey/3_airbnb/routes/userRouter.js
@@ -1,10 +1,11 @@
 const path = require('path')
+const rootDir = require("../utils/pathUtil")
 
 const express = require('express');
 const userRouter = express.Router();
 
 userRouter.get('/',(req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', "home.html")) 
+    res.sendFile(path.join(rootDir, 'views', "home.html")) 
 });
 
 module.exports = userRouter;
