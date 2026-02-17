3a19c4cbd0d9a695b32e94b07e6e7a5141f64f7d
vikashgitkit <vikashofficial9@gmail.com>
2026-02-17 12:04:27 +0530

    Serinv hrml files in node js

--- backend-dev-journey/3_airbnb/app.js
+++ backend-dev-journey/3_airbnb/app.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const express = require('express');
 const userRouter = require('./routes/userRouter')
 const hostRouter = require('./routes/hostRouter')
@@ -10,7 +12,7 @@ app.use(userRouter);
 app.use('/host',hostRouter);
 
 app.use((req,res,next)=> {
-    res.status(404).send("<h1>Your page is not found</h1>")
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 const PORT = 3000;
